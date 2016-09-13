57ef232cca0ab49123141c0f2579c2b6880cdb0b
Sam Saccone <samccone@gmail.com>
2016-09-12 20:57:01 -0700

    Add simple push vs non-push test case
    ref #1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var http2 = require('http2');
 var extractPush = require('./extract-push');
+var staticPrefix = 'static';
 
 function pushFiles(response, toPush) {
   toPush.forEach((v) => {
@@ -11,9 +12,9 @@ function pushFiles(response, toPush) {
   });
 }
 
-function handleRoot(request, response) {
+function handleDocument(request, response) {
   response.setHeader('content-type', 'text/html');
-  let indexPath = path.join(__dirname, '/static/index.html');
+  let indexPath = path.join(__dirname, request.url);
 
   if (response.push) {
     extractPush(
@@ -25,16 +26,25 @@ function handleRoot(request, response) {
   fs.createReadStream(indexPath).pipe(response);
 }
 
-function onRequest(request, response) {
-  switch(request.url) {
-    case '':
-    case '/':
-      handleRoot(request, response);
-      break;
-    default:
-      response.writeHead(404);
-      response.end();
+function handleStatic(request, response) {
+  if (request.url === '/') {
+    response.setHeader('content-type', 'text/html');
+    fs.createReadStream(path.join(__dirname, '/static/index.html')).pipe(response);
+    return;
   }
+
+  if (request.url.startsWith('/static/')) {
+    fs.createReadStream(path.join(__dirname, request.url)).pipe(response);
+    return;
+  }
+
+  response.writeHead(404);
+  response.end();
+}
+
+function onRequest(request, response) {
+  let fn = request.url.endsWith('.html') ? handleDocument : handleStatic;
+  fn(request, response);
 }
 
 server = http2.createServer({
--- static/sample.js
+++ static/sample.js
@@ -1,3 +1,5 @@
 (function() {
   console.log('hello from static');
+  performance.mark('js_executed');
+  performance.measure('time to run js', 'page_boot', 'js_executed');
 })();
