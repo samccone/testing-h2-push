eb6f3a3493f436cf3770cbc79e564dd730f07b53
Sam Saccone <samccone@google.com>
2016-09-15 21:04:46 -0700

    Use nav start instead of mark.

--- static/sample.js
+++ static/sample.js
@@ -1,5 +1,5 @@
 (function() {
   console.log('hello from static');
   performance.mark('js_executed');
-  performance.measure('time to run js', 'page_boot', 'js_executed');
+  performance.measure('time to run js', undefined, 'js_executed');
 })();
