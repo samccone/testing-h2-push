0e4f98a56b4e7c53b2f144a8c070e2f519c98ee7
Sam Saccone <samccone@google.com>
2016-09-15 20:27:26 -0700

    Implement basic batch gatherer.

--- runner/gather-times.js
+++ runner/gather-times.js
@@ -3,8 +3,53 @@ const path = require('path');
 const fs = require('fs');
 const config = JSON.parse(
     fs.readFileSync(path.join(__dirname, 'runner.json'), 'utf8'));
+const root = 'https://localhost:3000/static/';
+const SAMPLE_SIZE = 5;
+const SLEEP_TIME_MS = 50;
 
-lighthouse('https://localhost:3000/static/push-simple.html', {
-      loadPage: true,
-    },
-    config).then(console.log.bind(console));
+function extractTimingDuration(name, timings) {
+  return timings.extendedInfo.value.find(v => {
+    return v.name === name}).duration;
+}
+
+function getTiming(url, timingName) {
+  return lighthouse(`${root}/${url}`, {loadPage: true}, config).then((results) => {
+    return extractTimingDuration(
+        timingName,
+        results.audits['user-timings']);
+  }).catch(console.log.bind(console));
+}
+
+function gatherNTmes(
+    gather,
+    number,
+    sleepBetween=SLEEP_TIME_MS,
+    accumulator=[]) {
+
+  return gather().then(v => {
+    console.log(`run ${accumulator.length + 1} complete`);
+    accumulator.push(v);
+
+    if (accumulator.length < number) {
+      return new Promise((res, rej) => {
+       console.log(`sleeping for ${sleepBetween}ms`);
+        setTimeout(() => {
+          gatherNTmes(
+              gather,
+              number,
+              sleepBetween,
+              accumulator).then(res).catch(rej);
+        }, sleepBetween);
+      });
+    } else {
+      return accumulator;
+    }
+  })
+}
+
+gatherNTmes(
+    getTiming.bind(null, 'push-simple.html', 'time to run js'),
+    SAMPLE_SIZE,
+    SLEEP_TIME_MS).then((results) => {
+  console.log(results);
+});
