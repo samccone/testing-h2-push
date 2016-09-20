ea9cdf701567d4fb5b49a7455395b4eef9c30312
Sam Saccone <samccone@google.com>
2016-09-19 21:21:58 -0700

    Further simplify writing sequences

--- runner/gather-times.ts
+++ runner/gather-times.ts
@@ -10,11 +10,20 @@ const root:string = 'https://localhost:3000/static/';
 let outData:OutData = {};
 
 let boundGetTiming = (url: string, timingName: string) => getTiming(root, url, timingName)
+let boundSimplePush = () => gatherSimplePush(outData, boundGetTiming)
+let boundNoSimplePush = () => gatherNoSimplePush(outData, boundGetTiming)
 
-gatherSimplePush(outData, boundGetTiming).then(() => {
-  return gatherNoSimplePush(outData, boundGetTiming);
-}).then(() => {
-  return gatherNoSimplePush(outData, boundGetTiming);
-}).then(() => {
-  return gatherSimplePush(outData, boundGetTiming);
-}).then(() => console.log(printCSV(outData))).catch(console.log.bind(console));
+function chainGather(...promises: Array<() => Promise<any>>):Promise<any> {
+  return Array.from(arguments).reduce((prev, curr) => {
+    return prev.then(() => curr())
+  }, Promise.resolve());
+}
+
+chainGather(
+  boundSimplePush,
+  boundNoSimplePush,
+  boundNoSimplePush,
+  boundSimplePush
+).then(() => {
+  console.log(printCSV(outData));
+}).catch(console.log.bind(console));
