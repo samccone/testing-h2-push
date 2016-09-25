fed506f3fa447bedefdd17ef3188a8508030ab89
Sam Saccone <samccone@gmail.com>
2016-09-25 13:37:36 -0700

    Update simple test case params.

--- runner/simple-testcase.ts
+++ runner/simple-testcase.ts
@@ -1,7 +1,7 @@
 /// <reference path="typings/index.d.ts" />
 import {generateCase} from './test-case-base'
 
-const push = generateCase('push-simple.html', 'time to run js');
-const noPush = generateCase('no-push-simple.html', 'time to run js');
+const push = generateCase('push-simple.html', 'time to run js', 'simple w/ push');
+const noPush = generateCase('no-push-simple.html', 'time to run js', 'simple w/o push');
 
 export {push, noPush}
